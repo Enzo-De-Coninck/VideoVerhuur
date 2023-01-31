b1c303a1ad1ccf1aca4b6e5efbf86d1c25e6154c
Enzo <enzo.deconinck@vdabcampus.be>
2023-01-31 15:44:28 +0100

    services afgewerkt

---
+++ VideoVerhuur/Services/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using VideoData.Models;
+
+namespace VideoVerhuur.Services
+{
+    public interface IUserService
+    {
+        public void HuurFilm(Film film);
+        public List<Film> GetMandje();
+        public decimal GetTotaal();
+        public void VerwijderFilm(int id);
+        public Klant? GetUser();
+        public Klant? TryLogin(string Naam, string Postcode);
+        public bool IsIngelogd();
+        public void Logout();
+    }
+}
---
+++ VideoVerhuur/Services/UserService.cs
@@ -0,0 +1,61 @@
+﻿using VideoData.Models;
+
+namespace VideoVerhuur.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly VideoVerhuurDbContext _context;
+        private readonly List<Film> Winkelmandje;
+        private Klant? _user;
+
+        public UserService(VideoVerhuurDbContext context)
+        {
+            _context = context;
+            Winkelmandje = new();
+        }
+
+        public Klant? GetUser()
+        {
+            return _user;
+        }
+        public List<Film> GetMandje()
+        {
+            return Winkelmandje;
+        }
+
+        public void HuurFilm(Film film)
+        {
+            if (!Winkelmandje.Contains(film))
+                Winkelmandje.Add(film);
+        }
+        public void VerwijderFilm(int id)
+        {
+            Winkelmandje.Remove(Winkelmandje.First(film => film.FilmId == id));
+        }
+
+        public Decimal GetTotaal()
+        {
+            decimal totaal = 0m;
+            Winkelmandje.ForEach(f => totaal += f.Prijs);
+            return totaal;
+        }
+
+        public Klant? TryLogin(string Naam, string postcode)
+        {
+
+            return _user = _context.Klanten.FirstOrDefault(klant => klant.Naam == Naam && klant.PostCode == postcode);
+        }
+
+        public bool IsIngelogd()
+        {
+            if (_user == null)
+                return false;
+            return true;
+        }
+
+        public void Logout()
+        {
+            _user = null;
+        }
+    }
+}
